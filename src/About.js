a1503b052bacf81152b2791d9c6d950941a960a2
starrolle13 <starnesharolle13@gmail.com>
2022-02-11 14:43:29 -0800

    transitions

--- src/About.js
+++ src/About.js
@@ -6,15 +6,21 @@ function About() {
       </h1>
       <div id="aboutCardBox">
         <div className="topBox1"></div>
-        <div className="bottomBox"></div>
+        <div className="bottomBox">
+          <div className="aboutTitle">SCOTT</div>
+        </div>
       </div>
       <div id="aboutCardBox">
         <div className="topBox2"></div>
-        <div className="bottomBox"></div>
+        <div className="bottomBox">
+          <div className="aboutTitle">DERRICK</div>
+        </div>
       </div>
       <div id="aboutCardBox">
         <div className="topBox3"></div>
-        <div className="bottomBox"></div>
+        <div className="bottomBox">
+          <div className="aboutTitle">STAR</div>
+        </div>
       </div>
     </div>
   );
