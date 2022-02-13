15f7193dc09c9e30b4dcabf09d3ab05b194eb10e
dsump <dsumpmann@gmail.com>
2022-02-13 13:08:44 -0700

    update form working

--- src/Modal.js
+++ src/Modal.js
@@ -3,58 +3,14 @@ import './Modal.css'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-const Modal = ({setOpenModal}) => {
+const Modal = ({openModal, setOpenModal, formOpen, setFormOpen, editOpen, setEditOpen, exerciseProp }) => {
+    console.log("Exercise we passed in");
+    console.log(exerciseProp);
     
-    
-    const url = 'https://gitness-ga-earth-api.herokuapp.com/routines'
-   
-   
-    const handleChange = (event) => {
-        event.preventDefault()
-       
-        if(event.target.id === "routine_name"){
-            setFormState({ ...formState, [event.target.id]: event.target.value});
-        } else if(event.target.id === "routine_description") {
-            setFormState({ ...formState, [event.target.id]: event.target.value});
-        } else if (event.target.id === "reps.min"){
-            let tempForm = formState;
-            // tempForm.exercises[0].reps.minmax[0] = {...tempForm.exercises[0].reps.minmax[0], "reps.minmax": event.target.value}
-            tempForm.exercises[0].reps.minmax[0] = event.target.value;
-            setFormState({...tempForm});
-        } else if (event.target.id ==="reps.max") {
-          let tempForm = formState;
-          // tempForm.exercises[0].reps.minmax[0] = {...tempForm.exercises[0].reps.minmax[0], "reps.minmax": event.target.value}
-          tempForm.exercises[0].reps.minmax[1] = event.target.value;
-          setFormState({...tempForm});
-        } else {
-          let tempForm = formState;
-          tempForm.exercises[0] = {...tempForm.exercises[0], [event.target.id]: event.target.value}
-          // console.log(tempForm.exercises);
-          setFormState({...tempForm});
-        }
-        
-        //how to go about muscle groups and reps minmax
-        // console.log(formState);
-    }
 
-    const initFormState = {
-      routine_name: '',
-      routine_description: '',
-      exercises: [
-        {
-          exercise_name: '',
-          exercise_description: '',
-          reps: {
-            minmax: [0, 0]
-          },
-          sets: 0,
-          muscle_groups: '',
-          img_example: ''
-        }
-      ]
-    };
-  
-    const [formState, setFormState] = useState(initFormState);
+    const url = 'https://gitness-ga-earth-api.herokuapp.com/routines'
+    
+
   
     const handleSubmit = (event) => {
       event.preventDefault();
@@ -84,9 +40,55 @@ const Modal = ({setOpenModal}) => {
           console.error(error);
         });
     };
+   
+    const handleChange = (event) => {
+        event.preventDefault()
+       
+        if(event.target.id === "routine_name"){
+            setFormState({ ...formState, [event.target.id]: event.target.value});
+        } else if(event.target.id === "routine_description") {
+            setFormState({ ...formState, [event.target.id]: event.target.value});
+        } else if (event.target.id === "reps.min"){
+            let tempForm = formState;
+            // tempForm.exercises[0].reps.minmax[0] = {...tempForm.exercises[0].reps.minmax[0], "reps.minmax": event.target.value}
+            tempForm.exercises[0].reps.minmax[0] = event.target.value;
+            setFormState({...tempForm});
+        } else if (event.target.id ==="reps.max") {
+          let tempForm = formState;
+          // tempForm.exercises[0].reps.minmax[0] = {...tempForm.exercises[0].reps.minmax[0], "reps.minmax": event.target.value}
+          tempForm.exercises[0].reps.minmax[1] = event.target.value;
+          setFormState({...tempForm});
+        } else {
+          let tempForm = formState;
+          tempForm.exercises[0] = {...tempForm.exercises[0], [event.target.id]: event.target.value}
+          // console.log(tempForm.exercises);
+          setFormState({...tempForm});
+        }
+        
+        //how to go about muscle groups and reps minmax
+        // console.log(formState);
+    }
+    const initFormState = {
+      routine_name: '',
+      routine_description: '',
+      exercises: [
+        {
+          exercise_name: '',
+          exercise_description: '',
+          reps: {
+            minmax: [0, 0]
+          },
+          sets: 0,
+          muscle_groups: '',
+          img_example: ''
+        }
+      ]
+    };
+    const [ formState, setFormState] = useState(initFormState)
 
-
-    return(
+    const [exerciseEditState, setExerciseEditState] = useState(exerciseProp)
+    if (formOpen) {
+     return(
         <div className='modalBackground'>
           <div className='modalContainer'>
             <form onSubmit={handleSubmit}>
@@ -138,7 +140,50 @@ const Modal = ({setOpenModal}) => {
         </div>
         
     )
-}
+} else if (editOpen) {
+  return(
+    <div className='modalBackground'>
+          <div className='modalContainer'>
+              <form onSubmit={handleSubmit}>
+                      <div className='modaltitle'>
+                        <h2>Edit the exercise!</h2>
+                        <button type='button' onClick={() => {setOpenModal(false)}}>X</button>
+                      </div>
+                        <div className='modalbody'>
+                            <label> Exercises*:
+                              <br/>
+                              <label> Name:* 
+                              <input id='exercise_name' type='text' onChange={handleChange} value={exerciseEditState.exercise_name}/>
+                              </label> 
+                              <label> Description:*
+                              <input id='exercise_description' type='text' onChange={handleChange} value={exerciseEditState.exercise_description}/>
+                              </label> 
+                              <label> Sets:* 
+                              <input id='sets' type='text' onChange={handleChange} value={exerciseEditState.sets}/>
+                              </label> 
+                              <label> Min Reps:* 
+                              <input id='reps.min' type='text' onChange={handleChange} value={exerciseEditState.reps.minmax[0]}/>
+                              </label> 
+                              <label> Max Reps:* 
+                              <input id='reps.max' type='text' onChange={handleChange} value={exerciseEditState.reps.minmax[1]}/>
+                              </label> 
+                              <label> Muscle (Groups)*: 
+                              <input id='muscle_groups' type='text' onChange={handleChange} value={exerciseEditState.muscle_groups}/>
+                              </label> 
+                              <label> Image, Video, or GIF: 
+                              <input id='img_example' type='text' onChange={handleChange} value={exerciseEditState.img_example}/>
+                              </label> 
+                            </label> 
+                        </div>
+                        <div className='modalfooter'>
+                        <button type='submit' value='submit'/>
+                          <p> * = required field</p>        
+                        </div>
+                </form>
+          </div>
+      </div>
+  )
+}}
 
 
 export default Modal
--- src/RoutineDetails.js
+++ src/RoutineDetails.js
@@ -1,54 +1,140 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Nav from './Nav';
+import Modal from './Modal';
+import './Modal.css'
+
+/* TODO ==========================================
+1. Setup the modal so that it pops up when you click an edit button. DONE
+2. Make sure the modal contains the right exercise info to be changed. 
+3. Add a delete button to the modal that deletes the whole exercise
+4. The submit button should say "Submit Changes" and will do a patch/put request for that exercise in this routine. 
+5. When edit OR delete is clicked, the modal closes and the page is updated accordingly.
+==================================================*/
 
 function RoutineDetails() {
+  //Setup Modal props
+  const [ openModal, setOpenModal ] = useState(false);
+  const [ formOpen, setFormOpen ] = useState(false)
+  const [ editOpen, setEditOpen ] = useState(false)
+
   let { id } = useParams();
-  const [workout, setWorkout] = useState(null);
+  const [routine, setRoutine] = useState(null);
+  const [myExercise, setMyExercise] = useState(null);
   const url = `https://gitness-ga-earth-api.herokuapp.com/routines/${id}`;
+
+  const changeExercise = (exerciseID) => {
+    routine.exercises.forEach((exercise) => {
+      if (exercise._id === exerciseID) {
+        setMyExercise(exercise);
+        return;
+      }
+    });
+  };
 
   useEffect(() => {
     fetch(url)
       .then((res) => res.json())
       .then((json) => {
-        setWorkout(json);
+        setRoutine(json);
       })
       .catch(console.error);
   }, []);
 
-  console.log(workout);
-  if (!workout) {
-    return <p>Loading workout stuff...</p>;
-  } else {
-    return (
-      <div>
-        <h1>
-          ROUTINE <span id="yellowFont">DETAILS</span>
-        </h1>
-        <div className="routineDetailsBox">
-          <Nav />
+  
 
-          <div className="gifBox"></div>
-          <div className="details-container">
-            <div className="details">
-              <h2 className="routineName">{workout.routine_name}</h2>
-              <h4>Workout Description</h4>
-              <ol className="exercisesOL">
-                {workout.exercises.map((exercise) => {
-                  return (
-                    <li className="exercisesLI">
-                      {exercise.exercise_name} / Reps: min{' '}
-                      {exercise.reps.minmax[0]}, max {exercise.reps.minmax[1]} /
-                      Sets: {exercise.sets}
-                    </li>
-                  );
-                })}
-              </ol>
+  console.log(routine);
+  if (!routine) {
+    return <p>Loading routine...</p>;
+  } else {
+    if(!myExercise){ //if the user hasn't clicked an exercise yet, info pane has "Click an exercise!"
+      return (
+        <div>
+          <h1>
+            <span id="yellowFont">{routine.routine_name}</span>
+          </h1>
+          {/* <h2>{routine.routine_description}</h2> */}
+          <div className="routineDetailsBox">
+            <Nav />
+            
+            <div className="gifBox"></div>
+            <div className="details-container">
+              <div className="details">
+                {/* <h2 className="routineName">{routine.routine_name}</h2> */}
+                <h4>{routine.routine_description}</h4>
+                <ol className="exercisesOL">
+                  {routine.exercises.map((exercise) => {
+                    return (
+                      <li className="exercisesLI" key={exercise._id}>
+                        <button onClick={() => {
+                          changeExercise(exercise._id);
+                        }}>
+                          {exercise.exercise_name} / Reps: min{' '}
+                          {exercise.reps.minmax[0]}, max {exercise.reps.minmax[1]} /
+                          Sets: {exercise.sets}
+                        </button>
+                      </li>
+                    );
+                  })}
+                </ol>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    } else { //if the user has clicked an exercise, info pane has all the exercise details, including an edit button to pop up the modal
+      return (
+        <div>
+          <h1>
+            <span id="yellowFont">{routine.routine_name}</span>
+          </h1>
+          {/* <h2>{routine.routine_description}</h2> */}
+          <div className="routineDetailsBox">
+            <Nav />
+            {openModal && (
+              <Modal 
+                openModal={openModal}
+                setOpenModal={setOpenModal} 
+                formOpen={ formOpen }
+                setFormOpen={ setFormOpen }
+                editOpen={ editOpen }
+                setEditOpen={ setEditOpen }
+                exerciseProp={myExercise}/>
+            )}
+            <div className="gifBox"></div>
+            <div className="details-container">
+              <div className="details">
+                {/* <h2 className="routineName">{routine.routine_name}</h2> */}
+                <h4>{routine.routine_description}</h4>
+                <ol className="exercisesOL">
+                  {routine.exercises.map((exercise) => {
+                    return (
+                      <li className="exercisesLI" key={exercise._id}>
+                        <button onClick={() => {
+                          changeExercise(exercise._id);
+                        }}>
+                          {exercise.exercise_name} / Reps: min{' '}
+                          {exercise.reps.minmax[0]}, max {exercise.reps.minmax[1]} /
+                          Sets: {exercise.sets}
+                        </button>
+                        <button id="choiceYellow" onClick={() => {
+                          console.log("Clicked update button");
+                          setOpenModal(true)
+                          setEditOpen(true)
+                        }}>
+                          UPDATE
+                        </button>
+                      </li>
+                    );
+                  })}
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
   }
 }
 export default RoutineDetails;
+
